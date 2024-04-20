5179147279cbe0dd5f5dbce455f0ef16ee7bc2e6
jhinericusername <pikuaja@gmail.com>
2024-04-20 00:57:01 -0700

    venv

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 from fastapi import FastAPI
 from pymongo import MongoClient
-import os
 
 app = FastAPI()
 
 # Connect to MongoDB
-mongodb_url = os.environ.get("MONGODB_URL", "mongodb://localhost:27017")
-client = MongoClient(mongodb_url)
+client = MongoClient("mongodb://localhost:27017")
 db = client["mydatabase"]
 collection = db["mycollection"]
-
 
 @app.get("/")
 def read_root():
